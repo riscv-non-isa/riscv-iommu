7c446981c7af208c7f9b9a9f36fa490006c8e6a7
Ved Shanbhogue <ved@rivosinc.com>
2022-08-29 10:48:43 -0500

    updates

--- proposed_iommu_ref_model/libtables/include/tables_api.h
+++ proposed_iommu_ref_model/libtables/include/tables_api.h
@@ -11,9 +11,6 @@ uint64_t add_g_stage_pte(iohgatp_t iohgatp, uint64_t gpa, gpte_t gpte, uint8_t a
 uint64_t add_s_stage_pte(iosatp_t satp, uint64_t va, pte_t pte, uint8_t add_level);
 uint64_t add_vs_stage_pte(iosatp_t satp, uint64_t va, pte_t pte, uint8_t add_level, iohgatp_t iohgatp);
 uint64_t translate_gpa (iohgatp_t iohgatp, uint64_t gpa, uint64_t *spa);
-void print_dev_context(device_context_t *DC, uint32_t device_id);
-void print_process_context(process_context_t *DC, uint32_t device_id, uint32_t process_id);
-
 
 
 extern uint64_t get_free_ppn(uint64_t num_ppn);
